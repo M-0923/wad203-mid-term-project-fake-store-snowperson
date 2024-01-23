978054430ffc7366e9514a7c2708e21b3bbd6142
akio <124953279+akiomatic@users.noreply.github.com>
2024-01-22 16:28:06 -0800

    Install Vite (#8)
    * chore: modify .gitignore to include logs, build artifacts, and editor files
    
    * build: install Vite, keeping the same folder structure as the original
    
    * build: inject SCSS file directly, removing old compiled css file
    
    * build: add script 'npm run front' to run Vite individually
    
    ---------
    
    Co-authored-by: akio <124953279+akiomatic108@users.noreply.github.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import "/sass/index.scss";
 import { App } from "./App.js";
 
 $(() => {
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    // Change the root directory to the 'src' folder instead of the project root
+    root: "src",
+})
