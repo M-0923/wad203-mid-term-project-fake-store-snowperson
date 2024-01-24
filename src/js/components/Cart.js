3ea4a7584f202a5e463a7bb3be5b26421f87e779
Kai <kkai.chien0813@gmail.com>
2024-01-24 13:20:50 -0800

    feat: implement Cart class

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,5 +1,43 @@
 export class Cart {
-  getTotalPrice() {}
+  /**
+   * @type {string} #productId
+   */
+  #productId;
+  /**
+   * @type {string} #quantity
+   */
+  #quantity;
 
-  render() {}
+  /**
+   *
+   * @param {string} productId
+   */
+  constructor(productId) {
+    this.#productId = productId;
+    this.#quantity = 1;
+  }
+
+  /**
+   * Return #productId
+   * @returns {string} #productId
+   */
+  getProductId() {
+    return this.#productId;
+  }
+
+  /**
+   * Return #quantity
+   * @returns {string} #quantity
+   */
+  getQuantity() {
+    return this.#quantity;
+  }
+
+  increaseQuantity() {
+    return this.#quantity++;
+  }
+
+  decreaseQuantity() {
+    return this.#quantity--;
+  }
 }
