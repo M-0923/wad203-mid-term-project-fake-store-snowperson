3d67bbcef0be526e83d3d0372a0f2931cf805219
akio <124953279+akiomatic108@users.noreply.github.com>
2024-01-24 14:57:47 -0800

    feat: add singleton App class

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,66 @@
-export class App {
-  render() {}
+export default class App {
+  /**
+   * @type {Shop}
+   */
+  static #instance = null;
+
+  /**
+   * @type {ProductManager}
+   */
+  #productManager;
+
+  /**
+   * @type any
+   */
+  #cartManager;
+
+  /**
+   * Creates an instance of Shop.
+   */
+  constructor() {
+    if (App.#instance) {
+      return App.#instance;
+    }
+    App.INSTANCE = this;
+  }
+
+  /**
+   * Return the instance of Shop
+   * @returns {Shop}
+   */
+  static getInstance() {
+    return App.#instance;
+  }
+
+  /**
+   * Return the instance of ProductManager
+   * @returns {ProductManager}
+   */
+  getProductManager() {
+    return this.#productManager;
+  }
+
+  /**
+   * Return the instance of Cart
+   * @returns
+   */
+  getCartManager() {
+    return this.#cartManager;
+  }
+
+  /**
+   * Set the instance of ProductManager
+   * @param {ProductManager} productManager
+   */
+  setProductManager(productManager) {
+    this.#productManager = productManager;
+  }
+
+  /**
+   * Set the instance of Cart
+   * @param cartManager
+   */
+  setCartManager(cartManager) {
+    this.#cartManager = cartManager;
+  }
 }
