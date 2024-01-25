6bf37e61278a9948593d0bb51fda9ac54e43c2db
akio <124953279+akiomatic108@users.noreply.github.com>
2024-01-24 22:04:46 -0800

    docs: add JSDoc for Observable class

--- src/js/renderer/Observable.js
+++ src/js/renderer/Observable.js
@@ -1,20 +1,42 @@
+/**
+ * Observable class that can be extended to create an observable object.
+ * Observer pattern is used to notify the observers when the state of the observable object changes.
+ */
 export default class Observable {
+  /**
+   * @type {function[]}
+   */
   #observers;
 
+  /**
+   * Creates an instance of Observable.
+   */
   constructor() {
     this.#observers = [];
   }
 
+  /**
+   * Subscribes the given callback function to the observers.
+   * @param {function} callback
+   */
   // eslint-disable-next-line
-  subscribe(observer) {
-    this.#observers.push(observer);
+  subscribe(callback) {
+    this.#observers.push(callback);
   }
 
+  /**
+   * Unsubscribes the given callback function from the observers.
+   * @param {function} callback
+   */
   // eslint-disable-next-line
-  unsubscribe(observer) {
-    this.#observers = this.#observers.filter((obs) => obs !== observer);
+  unsubscribe(callback) {
+    this.#observers = this.#observers.filter((obs) => obs !== callback);
   }
 
+  /**
+   * Notifies/calls all the callback functions in the observers.
+   * @param {any} data
+   */
   // eslint-disable-next-line
   notify(data) {
     this.#observers.forEach((observer) => observer(data));
